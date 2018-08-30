0f6c9422e04af163ce07cdd2dc96b4cd98ec4068
Oli Zimpasser <oglimmer@gmail.com>
2018-08-30 23:48:49 +0200

    Add Fulgensfile.js

---
+++ Fulgensfile.js
@@ -0,0 +1,34 @@
+module.exports = {
+
+  config: {
+    Name: "DSA-Talent-Calc",
+    Vagrant: {
+      Box: 'ubuntu/xenial64',
+      Install: 'maven openjdk-8-jdk-headless docker.io'
+    },
+    JavaVersions: [ "1.8" ]
+  },
+
+  software: {
+
+    dsatalentcalc: {
+      Source: "mvn",
+      Git: "https://github.com/oglimmer/dsa-talent-calc.git",
+      Dir: "$$TMP$$/dsatalentcalc",
+      Mvn: {
+        Goal: "install"
+      }
+    },
+
+    dsatalentcalcweb: {
+      Source: "mvn",
+      Artifact: "target/dsa-talent-calc##001.war",
+    },
+
+    tomcat: {
+      Source: "tomcat",
+      Deploy: "dsatalentcalcweb"
+    }
+  }
+
+}
